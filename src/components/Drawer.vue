621fb33c55970b5e7af17a2bc99457b5fdb56162
littlebugyang <im.lby@qq.com>
2019-07-06 11:57:37 +0800

    在Drawer处添加对员工的岗位校验

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -50,6 +50,8 @@
     MdSnackbar
   } from 'vue-material/dist/components'
 
+  import Globals from '@/global.js'
+
   Vue.use(MdDrawer)
   Vue.use(MdToolbar)
   Vue.use(MdList)
@@ -69,16 +71,39 @@
           message: '暂未开放'
         })
       },
+      showUnauthorized(occupation) {
+        let chineseType = [
+          '游客',
+          '系统管理岗',
+          '供应商管理岗',
+          '产品录入岗',
+          '产品审核岗',
+          '产品配置岗'
+        ]
+        this.$snackbar({
+          message: `尚无权限进入 ${occupation} 界面! 当前对应岗位为: ${chineseType[Globals.staff.type]}`
+        })
+      },
       goToAnotherPart: function (name) {
         // todo: 检验是否有权限进入该part, 如果没有则emit一个参数false
+        const staffType = parseInt(Globals.staff.type)
+        console.log(staffType)
         let close = true
         switch (name) {
           case 'admin':
-            this.$router.push('/main/admin/userManage')
+            if (staffType === 1) {
+              this.$router.push('/main/admin/userManage')
+              break
+            }
+            this.showUnauthorized('系统管理员')
             break
-          case "supplier":
-            this.$router.push("/main/supplier/dataManage");
-            break;
+          case 'supplier':
+            if (staffType === 2) {
+              this.$router.push('/main/supplier/dataManage')
+              break
+            }
+            this.showUnauthorized('供应商管理岗')
+            break
           case 'personal':
             this.$router.push('/main/settings/personal')
             break
@@ -86,13 +111,25 @@
             this.$router.push('/main/settings/page')
             break
           case 'entry':
-            this.$router.push('/main/entry/input')
+            if (staffType === 3) {
+              this.$router.push('/main/entry/input')
+              break
+            }
+            this.showUnauthorized('产品录入岗')
             break
           case 'review':
-            this.$router.push('/main/review/assess')
+            if (staffType === 4) {
+              this.$router.push('/main/review/assess')
+              break
+            }
+            this.showUnauthorized('产品审核岗')
             break
           case 'config':
-            this.$router.push('/main/config/add')
+            if (staffType === 5) {
+              this.$router.push('/main/config/add')
+              break
+            }
+            this.showUnauthorized('产品配置岗')
             break
           default:
             close = false
