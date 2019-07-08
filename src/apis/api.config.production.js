5051a1f1418ebf1473e910035877af5ede7310ab
yangjianwei <05491e38620026511f2fc01f668fa946@huawei.com>
2019-07-09 00:42:16 +0800

    看看能不能走反向代理

--- src/apis/api.config.production.js
+++ src/apis/api.config.production.js
@@ -1,5 +1,5 @@
 // RESTful API要求在URL中标明API的版本
-const host = '' // 暂时未定
+const host = 'localhost:5000' // 暂时未定
 const version = '/api/'    // 先这样吧???
 
 // module.exports 指向的内容会被require完整读出, 此处是被 @/apis/apis.js完整读出
